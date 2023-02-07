22d922fe918cfac367ba6c537cfa5c4b00c8d2fe
moyeen_g <moyeengodyari@gmail.com>
2023-02-07 21:05:25 +0530

    third commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ function Welcome({ name }) {
     console.log({ name });
     return(
         <>
-            <h1>Hey ! { name }</h1>
+            <h1>Hey! { name }</h1>
             <h2>Welcome to Newton School.</h2>
         </>
     )
