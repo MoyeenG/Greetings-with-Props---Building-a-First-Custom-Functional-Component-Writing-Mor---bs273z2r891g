a67ea25e6d6813572f9288282e13ad0be524cfb4
moyeen_g <moyeengodyari@gmail.com>
2023-02-07 20:51:59 +0530

    third commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,11 +2,12 @@
 
 import React from "react";
 
-function Welcome({ props }) {
+function Welcome({ name }) {
+    console.log({ name });
     return(
         <>
-            <h1>Hey ! { props }</h1>
-            <h2>Welcome to Newton School</h2>
+            <h1>Hey ! { name }</h1>
+            <h2>Welcome to Newton School.</h2>
         </>
     )
 }
